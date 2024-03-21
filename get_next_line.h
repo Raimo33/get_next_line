73830e71b4f29ba6826925a8aea2dd02414f3b05
Raimo <104778891+Raimo33@users.noreply.github.com>
2024-03-21 18:17:49 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -25,7 +25,6 @@ char	*get_next_line(int fd);
 char	*get_single_line(char *str);
 char	*f_strjoin(char *s1, char *s2);
 int		f_sl(char *c);
-char	*ft_calloc(size_t nmemb, size_t size);
 char	*free_and_null(char *to_free, char **to_null);
 
 #endif
